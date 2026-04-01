199e21c284e29be491a52047d7da1538e9cdf65a
Gia Huy Nguyen <nggiahuy09@gmail.com>
2026-04-01 21:59:25 +0700

    Initial Commit

---
+++ cooking_book/cooking_bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  cooking_bookApp.swift
+//  cooking_book
+//
+//  Created by Nguyễn Gia Huy on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct cooking_bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
