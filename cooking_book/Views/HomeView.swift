99b7df6b8fb6ac3d620a78f71fd9ecd91ee6b526
Gia Huy Nguyen <nggiahuy09@gmail.com>
2026-04-07 21:53:21 +0700

    feat(home): implement receipe items row without built-in grid view

--- cooking_book/Views/HomeView.swift
+++ cooking_book/Views/HomeView.swift
@@ -12,27 +12,55 @@ struct HomeView: View {
     @StateObject private var viewModel = HomeViewModel()
     @EnvironmentObject var sessionManager: SessionManager
 
-    var body: some View {
-        NavigationStack {
-            VStack {
-                Text("Cooking Book App!")
-            }
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing, content: {
-                    Button {
-                        viewModel.showSignOutAlert = true
-                    } label: {
-                        Image(systemName: "gearshape.fill")
-                            .foregroundStyle(.black)
-                    }
-                })
-            }
-            .alert("Are you sure you would like to sign out?", isPresented: $viewModel.showSignOutAlert) {
-                Button("Sign Out", role: .destructive) {
-                    sessionManager.sessionState = .loggedOut
-                }
+    fileprivate func ReceipeRowItem(receipe: Receipe, itemHeight: CGFloat, itemWidth: CGFloat) -> some View {
+        VStack(alignment: .leading, spacing: 8.0) {
+            Image(receipe.image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: itemWidth, height: itemHeight)
+                .cornerRadius(12.0)
+            Text(receipe.name)
+                .lineLimit(1)
+                .font(.system(size: 16.0, weight: .semibold))
+        }
+    }
 
-                Button("Cancel", role: .cancel) {}
+    let spacing: CGFloat = 4.0
+    let padding: CGFloat = 12.0
+
+    var body: some View {
+        GeometryReader { proxy in
+            let screenWidth = proxy.size.width
+            let itemWidth = (screenWidth - (spacing * 3) - (padding * 2)) / 3
+            let itemHeight = 1.3 * itemWidth
+
+            NavigationStack {
+                VStack {
+                    HStack {
+                        ForEach(0...2, id: \.self) { index in
+                            ReceipeRowItem(receipe: Receipe.mockReceipes[index], itemHeight: itemHeight, itemWidth: itemWidth)
+                        }
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal, padding)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing, content: {
+                        Button {
+                            viewModel.showSignOutAlert = true
+                        } label: {
+                            Image(systemName: "gearshape.fill")
+                                .foregroundStyle(.black)
+                        }
+                    })
+                }
+                .alert("Are you sure you would like to sign out?", isPresented: $viewModel.showSignOutAlert) {
+                    Button("Sign Out", role: .destructive) {
+                        sessionManager.sessionState = .loggedOut
+                    }
+
+                    Button("Cancel", role: .cancel) {}
+                }
             }
         }
     }
